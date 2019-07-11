4ec5d9396e752e62ad7813791a1f346f0cbfcfcb
Principerebelde <52800816+Principerebelde@users.noreply.github.com>
2019-07-11 15:10:40 -0500

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,8 @@
+function dato()
+{
+	var a=String(document.getElementById('v1').value);
+	var b=String(document.getElementById('v2').value);
+
+	document.getElementById('usuario').value=a;
+	document.getElementById('email').value=b;
+}
